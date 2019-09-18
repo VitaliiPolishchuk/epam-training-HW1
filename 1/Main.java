4f7437c74f23d08e6ac191dbeee33b10640fe423
Vitalii <37402578+VitaliiPolishchuk@users.noreply.github.com>
2019-09-19 01:04:50 +0300

    update 1 task with {}

--- 1/Main.java
+++ 1/Main.java
@@ -8,8 +8,12 @@ public class Main {
         while (true) {
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
                 String binaryNumber = reader.readLine();
-                if(!isValidBinaryNumber(binaryNumber)) System.out.println("Error, not valid binary number");
-                else System.out.println(convertBinaryToDecimal(binaryNumber));
+                if(!isValidBinaryNumber(binaryNumber)) {
+                    System.out.println("Error, not valid binary number");
+                }
+                else {
+                    System.out.println(convertBinaryToDecimal(binaryNumber));
+                }
             } catch (Exception e) {
                 System.out.println("Error during reading input");
             }
@@ -19,18 +23,22 @@ public class Main {
 
     private static boolean isValidBinaryNumber(String binaryNumber) {
         if (binaryNumber.length() > Integer.SIZE) return false;
-        for (int i = 0; i < binaryNumber.length(); ++i)
+        for (int i = 0; i < binaryNumber.length(); ++i) {
             if (binaryNumber.charAt(i) != '0' &&
-                binaryNumber.charAt(i) != '1')
+                binaryNumber.charAt(i) != '1') {
                 return false;
+            }
+        }
         return true;
     }
 
     private static int convertBinaryToDecimal(String binaryNumber){
         int res = 0, n = binaryNumber.length();
-        for (int i = 0; i < n; ++i)
-            if (binaryNumber.charAt(i) == '1')
-                res += 1 << (n - i - 1);
+        for (int i = 0; i < n; ++i) {
+            if (binaryNumber.charAt(i) == '1') {
+                res += 1 << (n - i - 1); 
+            }
+        }
         return res;
     }
 }
